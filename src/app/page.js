6f6ebe89452b8e18f71857a7720c4a335b4566f0
Anawar Hossain <anwarhossainhossain01@gmail.com>
2026-04-24 23:40:36 +0600

    Add daisyUI integration

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>This is Main page</h1>
+      <h1 className="text-green-700">This is Main page</h1>
+      <button className="btn">test</button>
     </div>
   );
 }
